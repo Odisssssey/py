829f4eda78072c246e284f48373dcfbc82bd6fb6
Odisssssey <Good_man5@mail.ru>
2017-01-09 17:21:33 +0300

    new
    new

--- numbers.py
+++ numbers.py
@@ -5,11 +5,10 @@ myNumber = 0
 while myNumber < 1 or myNumber > 10:
   myNumber = int(input('Введите число от 1 до 10: '))
 number = randint(1, 10)
-stop = 1
-while stop == 1:
+while myNumber:
   if myNumber == number:
     print("Вы угадали! ПК загадал число", number)
-    stop = 0
+    break
   else:
       if myNumber < number:
         print('ПК загадал число, больше введенного вами. Количество ошибок сделанное вами:', i)
